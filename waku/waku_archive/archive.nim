11461aed44e777ffde5655405c8fbce9c0c2e9c3
Ivan FB <128452529+Ivansete-status@users.noreply.github.com>
2026-03-19 21:37:04 +0100

    allow union of several retention policies (#3766)
    * refactor retention policy to allow union of several retention policies
    * bug fix time retention policy
    * add removal of orphan partitions if any
    * use nim-http-utils 0.4.1

--- waku/waku_archive/archive.nim
+++ waku/waku_archive/archive.nim
@@ -45,7 +45,7 @@ type WakuArchive* = ref object
 
   validator: MessageValidator
 
-  retentionPolicy: Option[RetentionPolicy]
+  retentionPolicies: seq[RetentionPolicy]
 
   retentionPolicyHandle: Future[void]
   metricsHandle: Future[void]
@@ -72,13 +72,17 @@ proc new*(
     T: type WakuArchive,
     driver: ArchiveDriver,
     validator: MessageValidator = validate,
-    retentionPolicy = none(RetentionPolicy),
+    retentionPolicies = newSeq[RetentionPolicy](0),
 ): Result[T, string] =
   if driver.isNil():
     return err("archive driver is Nil")
 
-  let archive =
-    WakuArchive(driver: driver, validator: validator, retentionPolicy: retentionPolicy)
+  if retentionPolicies.len == 0:
+    return err("at least one retention policy must be provided")
+
+  let archive = WakuArchive(
+    driver: driver, validator: validator, retentionPolicies: retentionPolicies
+  )
 
   return ok(archive)
 
@@ -253,16 +257,15 @@ proc findMessages*(
   )
 
 proc periodicRetentionPolicy(self: WakuArchive) {.async.} =
-  let policy = self.retentionPolicy.get()
-
   while true:
-    info "executing message retention policy"
-    (await policy.execute(self.driver)).isOkOr:
-      waku_archive_errors.inc(labelValues = [retPolicyFailure])
-      error "failed execution of retention policy", error = error
-      await sleepAsync(WakuArchiveDefaultRetentionPolicyIntervalWhenError)
-      ## in case of error, let's try again faster
-      continue
+    for policy in self.retentionPolicies:
+      info "executing message retention policy", policy = $policy
+      (await policy.execute(self.driver)).isOkOr:
+        waku_archive_errors.inc(labelValues = [retPolicyFailure])
+        error "failed execution of retention policy", policy = $policy, error = error
+        await sleepAsync(WakuArchiveDefaultRetentionPolicyIntervalWhenError)
+        ## in case of error, let's try again faster
+        continue
 
     await sleepAsync(WakuArchiveDefaultRetentionPolicyInterval)
 
@@ -279,7 +282,7 @@ proc periodicMetricReport(self: WakuArchive) {.async.} =
     await sleepAsync(WakuArchiveDefaultMetricsReportInterval)
 
 proc start*(self: WakuArchive) =
-  if self.retentionPolicy.isSome():
+  if self.retentionPolicies.len > 0:
     self.retentionPolicyHandle = self.periodicRetentionPolicy()
 
   self.metricsHandle = self.periodicMetricReport()
@@ -287,7 +290,7 @@ proc start*(self: WakuArchive) =
 proc stopWait*(self: WakuArchive) {.async.} =
   var futures: seq[Future[void]]
 
-  if self.retentionPolicy.isSome() and not self.retentionPolicyHandle.isNil():
+  if not self.retentionPolicyHandle.isNil():
     futures.add(self.retentionPolicyHandle.cancelAndWait())
 
   if not self.metricsHandle.isNil:
